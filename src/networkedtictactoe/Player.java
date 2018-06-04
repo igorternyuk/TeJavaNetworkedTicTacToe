555c78d8e51b7c7d6eccd2cdc5353dafaea2854a
igorternyuk <mr.ternyuk@mail.ru>
2018-06-04 15:10:29 +0300

    Loading images

--- src/networkedtictactoe/Player.java
+++ src/networkedtictactoe/Player.java
@@ -1,13 +1,16 @@
 package networkedtictactoe;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Graphics;
-import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.BoxLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,19 +22,27 @@ import javax.swing.JTextField;
  * @author igor
  */
 public class Player {
-    private static final int WINDOW_WIDTH = 600;
-    private static final int WINDOW_HEIGHT = 600;
+    private static final String TITLE_OF_PROGRAM = "NetworkedTicTacToe";
     private static final int SPOT_SIZE = 160;
     private static final int SPOT_GAP = 10;
-    private static final int FIELD_SIZE = 3;
+    private static final int BOARD_SIZE = 3;
+    private static final int CANVAS_SIZE = (SPOT_SIZE + SPOT_GAP) * BOARD_SIZE;
+    private static final int CONNECTION_PANEL_HEIGHT = 40;
+    private static final int WINDOW_WIDTH = CANVAS_SIZE - 7;
+    private static final int WINDOW_HEIGHT = CANVAS_SIZE + CONNECTION_PANEL_HEIGHT;
+    
     private JFrame window;
     private Canvas canvas;
     private JPanel connectionPanel;
     private JTextField textFieldHost;
     private JTextField textFieldPort;
     private JButton btnConnectToServer;
-    private static final String TITLE_OF_PROGRAM = "NetworkedTicTacToe";
-    
+    private String[][] board = {
+        { "X", "O", " " },
+        { " ", "X", " " },
+        { " ", " ", " " }
+    };
+    private boolean isCircle = false;
     
     public Player(){
         this.window = new JFrame(TITLE_OF_PROGRAM);
@@ -39,47 +50,78 @@ public class Player {
         this.connectionPanel = new JPanel(new GridLayout(1, 5));
         this.textFieldHost = new JTextField("127.0.0.1");
         this.textFieldPort = new JTextField("55555");
-        this.btnConnectToServer = new JButton("Connect to server");
+        this.btnConnectToServer = new JButton("Connect");
         setupGUI();
     }
     
     private void setupGUI(){
         this.window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.canvas.setSize((SPOT_SIZE + SPOT_GAP) * FIELD_SIZE,
-                (SPOT_SIZE + SPOT_GAP) * FIELD_SIZE);
-        Container container = this.window.getContentPane();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS)); 
-        this.connectionPanel.add(new JLabel("Server"));
+        this.canvas.setSize(CANVAS_SIZE, CANVAS_SIZE);
+        this.connectionPanel.add(new JLabel("Server:"));
         this.connectionPanel.add(this.textFieldHost);
         this.connectionPanel.add(new JLabel("Port:"));
         this.connectionPanel.add(this.textFieldPort);
         this.connectionPanel.add(this.btnConnectToServer);
-        this.connectionPanel.setSize(WINDOW_WIDTH,
-                WINDOW_HEIGHT - (SPOT_SIZE + SPOT_GAP) * FIELD_SIZE);
-        container.add(this.canvas);
-        container.add(this.connectionPanel);
+        this.window.add(BorderLayout.CENTER, this.canvas);
+        this.window.add(BorderLayout.SOUTH, this.connectionPanel);
         this.window.setResizable(false);
         this.window.setLocationRelativeTo(null);
         this.window.setVisible(true);
     }
     
-    private void loadImages(){
-        //BufferedImage 
-    }
-    
     private class Canvas extends JPanel implements MouseListener{
-        
+        private BufferedImage imgBoard;
+        private BufferedImage imgRedX;
+        private BufferedImage imgBlueX;
+        private BufferedImage imgRedO;
+        private BufferedImage imgBlueO;
+
         public Canvas(){
             setFocusable(true);
             requestFocus();
-            setBackground(Color.GREEN);
+            setBackground(Color.white);
             addMouseListener(this);
+            loadImages();
+        }
+        
+        private void loadImages(){
+            try {
+                this.imgBoard = ImageIO.read(getClass().getResource("images/board.png"));
+                this.imgRedX = ImageIO.read(getClass().getResource("images/redX.png"));
+                this.imgBlueX = ImageIO.read(getClass().getResource("images/blueX.png"));
+                this.imgRedO = ImageIO.read(getClass().getResource("images/redCircle.png"));
+                this.imgBlueO = ImageIO.read(getClass().getResource("images/blueCircle.png"));
+            } catch (IOException ex) {
+                System.out.println("Could not load image: " + ex.getMessage());
+                Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null,
+                        ex);
+            }
         }
         
         @Override
         public void paintComponent(Graphics g){
-            //super.paintComponent(g);
+            super.paintComponent(g);
+            g.drawImage(imgBoard, 0, 0, null);
+            for(int i = 0; i < board.length; ++i){
+                for(int j = 0; j < board[i].length; ++j){
+                    if("X".equalsIgnoreCase(board[i][j])){
+                        if(isCircle)
+                            g.drawImage(imgRedX, j * (SPOT_SIZE + SPOT_GAP),
+                                        i * (SPOT_SIZE + SPOT_GAP), null);
+                        else
+                            g.drawImage(imgBlueX, j * (SPOT_SIZE + SPOT_GAP),
+                                        i * (SPOT_SIZE + SPOT_GAP), null);
+                    } else if("O".equalsIgnoreCase(board[i][j])){
+                        if(isCircle)
+                            g.drawImage(imgBlueO,  j * (SPOT_SIZE + SPOT_GAP),
+                                        i * (SPOT_SIZE + SPOT_GAP), null);
+                        else
+                            g.drawImage(imgRedO,  j * (SPOT_SIZE + SPOT_GAP),
+                                        i * (SPOT_SIZE + SPOT_GAP), null);
+                    }
+                }
+            }
         }
         
         @Override
@@ -93,7 +135,8 @@ public class Player {
         @Override
         public void mouseReleased(MouseEvent e) {
             System.out.println("Mouse release event");
-            System.out.println("x = " + e.getX() + " y = " + e.getY());
+            System.out.println("x = " + e.getX() / (SPOT_SIZE + SPOT_GAP)
+                             + " y = " + e.getY() / (SPOT_SIZE + SPOT_GAP));
         }
 
         @Override
