0ff25653ffac0982b476a4f57e84d9db4ab59d9e
igorternyuk <mr.ternyuk@mail.ru>
2018-06-04 09:36:34 +0300

    first commit

---
+++ src/networkedtictactoe/NetworkedTicTacToe.java
@@ -0,0 +1,16 @@
+package networkedtictactoe;
+
+/**
+ *
+ * @author igor
+ */
+public class NetworkedTicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
